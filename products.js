99c4f1fb78da16fcb57d0558154f1c7bf9cccb29
Thanh Nay <218096274+tin10n@users.noreply.github.com>
2025-10-02 10:52:38 -0400

    updated products naming convention

--- products.js
+++ products.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
       return response.json();
     })
     .then((data) => {
-      const grid = document.getElementById("product-grid");
+      const grid = document.getElementById("product--grid");
 
 // Looping through each product in the array, as well as creating a div for each product.
       data.forEach((product) => {
@@ -19,19 +19,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Adding the product HTML inside the div.
         item.innerHTML = `
-                    <div class="item-img">
+                    <div class="item--img">
                         <a href="${product.Link}" target="_blank">
                             <img src="${product.Photo}" alt="${product.Name}">
                         </a>
-                        <div class="item-action">
-                            <a href="#" class="buy"><span><i class="fas fa-cart-plus"></i></span></a>
-                            <a href="#" class="wishlist"><span><i class="fas fa-heart"></i></span></a>
+                        <div class="item--action">
+                            <a href="#" class="buy" aria-label="Add ${product.Name} to cart"><span><i class="fas fa-cart-plus"></i></span></a>
+                            <a href="#" class="wishlist" aria-label="Add ${product.Name} to wishlist"><span><i class="fas fa-heart"></i></span></a>
                         </div>
                     </div>
-                    <div class="item-details">
-                        <p class="price">${product.Name}</p>
-                        <p class="name">${product.Price}</p>
-                        <p class="price">${product.Description}</p>
+                    <div class="item--details">
+                        <p class="item--name">${product.Name}</p>
+                        <p class="item--price">${product.Price}</p>
+                        <p class="item--desc">${product.Description}</p>
                     </div>
                 `;
 // Adding each item into the grid.
